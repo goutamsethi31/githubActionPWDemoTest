23aa76d44188dd8fd523dee0b401bc8129c617f5
goutam <goutamsethi31@gmail.com>
2026-01-17 16:26:29 +0530

    first commit

---
+++ tests/githubActionPWTest.spec.ts
@@ -0,0 +1,10 @@
+import {test,expect} from"@playwright/test";
+
+test("GitHub Action Test",async({page})=>{
+    await page.goto("https://demowebshop.tricentis.com/");
+    const title=await page.title();
+    expect(title).toBe("Demo Web Shop");
+
+    const productTitle= await page.locator(".product-title").first().textContent();
+    console.log(`First product title: ${productTitle}`);
+});
